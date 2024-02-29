c99a605221be8d46a1aceaf8df0655ad3f8bb62b
baxromjon <gitbahromjon77@gmail.com>
2024-02-29 22:41:59 +0300

    2  commit

--- sontop.py
+++ sontop.py
@@ -1,10 +1,13 @@
 import random
 
-n= random.randint(0,10)
+n= random.randint(0,5)
 
 guesst = int(input("0 va 10 orasidagi ihtiyoriy sonni kiriting:"))
 
-if n == guesst:
-    print("Topdingiz")
-else:
-    print("Topaolmadingiz")
+while True:
+    if n == guesst:
+        print("Topdingizshekili000")
+        break
+
+
+
